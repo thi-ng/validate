0b7026e4582a3e6262d0bf51ecfd3a3944ea89f5
Karsten Schmidt <k@postspectacular.com>
2013-05-22 15:04:27 +0100

    adding tests

---
+++ test/thi/ng/validate/test/core.clj
@@ -0,0 +1,20 @@
+(ns thi.ng.validate.test.core
+  (:require [thi.ng.validate.core :as v])
+  (:use clojure.test))
+
+(def db 
+  {:people [{:name "toxi" :age "38"}
+            {:name "foo" :city "london" :age 23}]})
+
+(deftest wildcard-req-keys
+  (let [specs [:people {:* [(v/map) (v/required-keys [:name :age :city])]}]
+        [m err] (apply v/validate db specs)]
+    (is (not (nil? (get-in err [:people 0]))))
+    (is (nil? (get-in err [:people 1])))))
+
+(deftest wildcard-age-correct
+  (let [specs [:people {:* {:age [(v/pos #(if (string? %) (read-string %))) (v/less-than 35)]}}]
+        [m err] (apply v/validate db specs)]
+    (is (= "must be less than 35" (first (get-in err [:people 0 :age]))))
+    (is (nil? (get-in err [:people 1])))
+    ))
