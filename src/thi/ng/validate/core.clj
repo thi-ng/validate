90cf9f3006f12c7953d8df0445ccd58924ba0c37
Karsten Schmidt <k@postspectacular.com>
2013-05-29 00:05:15 +0100

    lifting email & url regexps into own symbols

--- src/thi/ng/validate/core.clj
+++ src/thi/ng/validate/core.clj
@@ -283,10 +283,16 @@
   ([re] (matches re "must match regexp"))
   ([re msg] [(fn [_ v] (re-matches re v)) msg]))
 
+(def email-regexp
+  #"(?i)^[\w.%+-]+@[a-z0-9.-]+.[a-z]{2,6}$")
+
+(def url-regexp
+  #"(?i)^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$")
+
 (defn email
   "Returns validation spec for email addresses."
   ([] (email "must be a valid email address"))
-  ([msg] (matches #"(?i)^[\w.%+-]+@[a-z0-9.-]+.[a-z]{2,6}$" msg)))
+  ([msg] (matches email-regexp msg)))
 
 (defn url
   "Returns validation spec for URLs using comprehensive regex
@@ -295,5 +301,4 @@
   * <https://gist.github.com/dperini/729294>
   * <http://mathiasbynens.be/demo/url-regex>"
   ([] (url "must be a valid URL"))
-  ([msg]
-     (matches #"(?i)^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$" msg)))
+  ([msg] (matches url-regexp msg)))
