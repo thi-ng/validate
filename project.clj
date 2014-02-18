d71855b39d83761dac2e1fb73ba9426672b628eb
Karsten Schmidt <k@postspectacular.com>
2014-02-18 01:02:20 +0000

    add POM additions to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "https://github.com/thi-ng/validate"
   :license {:name "Apache Software License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :scm {:url "git@github.com:thi-ng/validate.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Karsten Schmidt"]
+                              [:url "http://thi.ng"]
+                              [:email "k@thi.ng"]
+                              [:timezone "0"]]])
